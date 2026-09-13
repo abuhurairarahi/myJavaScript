ffd9a434394edca0c135053bb5759d4068dc42b2
abuhurairarahi <mabuhurairarahi@gmail.com>
2026-09-13 12:37:10 +0600

    Syntax Added

---
+++ Conditions/Syntax.md
@@ -0,0 +1,8 @@
+#Comparsion Operators:
+
+1. > greater than
+2. < less than
+3. >= greater equals to
+4. <= less equals to
+5. === equals to
+6. !== not equals to
---
+++ Syntax.js
@@ -0,0 +1,10 @@
+// console printing
+console.log('Printing in the console');
+
+// asking data-type of a variable;
+let randomVariable = 10.34;
+let dataTypeName;
+
+dataTypeName = typeof randomVariable;
+console.log(dataTypeName);  // output: Number
+
