154a60d8f2af1fd614182a5506d3dafdd1ea915a
abuhurairarahi <mabuhurairarahi@gmail.com>
2026-09-13 12:15:35 +0600

    Initial commit

---
+++ String.js
@@ -0,0 +1,7 @@
+// Consentration: By using quotation and connect string with + like java
+
+'Total Cost: $' + (5+3);
+
+// Interpolation: under a pair of back-ticks connect the strings & (numbers + Math calculation) with ${}
+
+`Total Cost: $${5+3}`;
