bbb446ef2973a1ecfa7ddf471f6445f9add700d2
abuhurairarahi <mabuhurairarahi@gmail.com>
2026-09-26 00:08:39 +0600

    Add Comparison Operators

---
+++ comparison-operators.js
@@ -0,0 +1,19 @@
+//basic operators:
+console.log(5 > 1); //greater than
+console.log(5 < 1); //less than
+console.log(5 != 1); //not equal
+console.log(5 >= 1); //greater equal than
+console.log(5 <= 1); //less equal than
+
+console.log("5" == 5);
+/*
+    checks the if string-value inside the string and the number-value on the other hand matches
+    then it comes "true" else "false" 
+*/
+
+console.log("5" === 5);
+/*
+    it matchs the data-type of the both sides, if matches then comes "true" else "false"
+*/
+
+
